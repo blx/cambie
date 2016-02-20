9c017aad0bcb7a5738f3fc558171feacdf679694
Ben Cook <b@bencook.ca>
2016-02-20 03:45:27 -0800

    declarative argh

--- cambie/__main__.py
+++ cambie/__main__.py
@@ -1,156 +1,24 @@
 #!/usr/bin/env python2
 
-"""Loads CSVs of your travel history exported from TransLink BC's
-Compass Card site into an SQLite database."""
+from .argh import Command, argh
+from .cambie import load_trips, geocode_locations, plot_trips_by_hour
+from .util import connect
 
-from __future__ import print_function
+_db = lambda env: connect(env['db'])
 
-import time
-from datetime import datetime
-
-from .prelude import *
-
-from . import credentials as creds
-from .util import csv_rows, connect, create_table_once
-from . import translink_geocode as translink
-
-CSV_DIR = 'data'
-DB = 'compass.sqlite3'
-
-info = print
-get_bus_stop = partial(translink.get_stop, creds.API_KEY)
-
-DDL = {
-    'trip':       """create table "trip" (
-                       "id" int primary key,
-                       "datetime" text,
-                       "date" text,
-                       "time" text,
-                       "location" text,    -- like "50268" or "Waterfront Stn"
-                       "transaction" text,
-                       "product" text,
-                       "amount" int,
-                       unique("date", "time", "location", "transaction"))""",
-    'location':   """create table "location" (
-                       "id" int primary key,
-                       "location" text,    -- like "50187"
-                       "name" text,        -- like "WB W HASTINGS ST FS ABBOTT ST"
-                       "lat" decimal,
-                       "long" decimal)""",
-    'route':      """create table "route" (
-                       "id" int primary key,
-                       "name" text)        -- like "009" """,
-    'route_stop': """create table "route_stop" (
-                       "route_id" int,
-                       "location_id" int,
-                       foreign key("route_id") references route("id"),
-                       foreign key("location_id") references location("id"))"""
+commands = {
+    'load_trips':    Command(load_trips, deps=[_db], args=['csv_dir']),
+    'geocode_stops': Command(geocode_locations, deps=[_db]),
+    'plot_trips':    Command(plot_trips_by_hour, deps=[_db]),
 }
 
-def geocode_locations(db):
-    "Adds geo-information to db for all trips that haven't yet been geocoded."
-    create_table_once(db, DDL['location'])
+cli = argh('cambie', commands)
 
-    cur = db.cursor()
-    cur.execute("""select distinct(t."location") from "trip" t
-                   left outer join "location" l on l."location" = t."location"
-                   where l."location" is null
-                   and t."location" regexp '[0-9]+'""")
-    locs = mapv(first, cur)
-
-    for i, loc in enumerate(locs):
-        geo = get_bus_stop(loc)
-        result = (loc, geo['Name'].rstrip('- '), geo['Latitude'], geo['Longitude'])
-        cur.execute("""insert into "location" ("location", "name", "lat", "long")
-                       values (?, ?, ?, ?)""",
-                    result)
-        info('Geocoded %s (%s) to %0.6f, %0.6f' % result)
-
-        # TransLink limits API calls to 100/minute
-        if (i + 1) % 100 == 0:
-            t = 50
-            info('... Sleeping %d seconds to keep TransLink API happy ...' % t)
-            time.sleep(t)
-
-    db.commit()
-
-def ingest_csv(db, csvpath):
-    """Loads trip history from CSV into SQLite. Note: idempotent but unintelligent:
-    if you pass in a CSV that's already been ingested, the DB will be unchanged
-    but we'll still traverse the entire file."""
-    def parse_datetime(s):
-        dt = datetime.strptime(s, "%b-%d-%Y %I:%M %p")
-        # Split into date (2016-02-15) and time (09:31) components for sqlite.
-        # (Compass card datetime resolution is in minutes)
-        return [str(dt), str(dt.date()), str(dt.time())[:5]]
-
-    def rowfn(row):
-        row = parse_datetime(row[0]) + row[1:]
-        return row
-
-    cur = db.cursor()
-    # Insert if new; skip and ignore constraint violation if already exists
-    cur.executemany("""insert or ignore into trip
-                       ("datetime", "date", "time", "location", "transaction", "product", "amount")
-                       values (?, ?, ?, ?, ?, ?, ?)""",
-                    map(rowfn, csv_rows(csvpath)))
-    db.commit()
-
-def all_trips(db):
-    cur = db.cursor()
-    for row in cur.execute("""select * from "trip"
-                              where "transaction" != 'Missing Tap out'
-                              order by datetime"""):
-        yield row
-
-def load_trips(db, csv_dir):
-    import glob
-    import os
-
-    create_table_once(db, DDL['trip'])
-
-    for csvf in glob.iglob(os.path.join(csv_dir, '*.csv')):
-        info('Ingesting trip history from %s' % csvf)
-        ingest_csv(db, csvf)
 
 if __name__ == '__main__':
-    import argparse
-    from collections import namedtuple
+    import json
 
-    from . import vis
+    with open('env.json') as fp:
+        env = json.load(fp)
 
-    def plot_trips_by_hour(db):
-        import matplotlib.pyplot as plt
-        fig = vis.trips_by_hour(db)
-        plt.show()
-
-    def wrap_cmd(fn, args=None):
-        """Wrapper function to supply DB connection and appropriate arguments
-        to subcommands."""
-        return lambda ns: fn(connect(DB),
-                             **{k: getattr(ns, k)
-                                for k in args or []})
-
-    Command = namedtuple('Command', ['action', 'args'])
-
-    def load_cmds(argparser, cmds):
-        _cmds = argparser.add_subparsers()
-
-        for name, [action, args] in items(cmds):
-            cmd = _cmds.add_parser(name)
-            args = args or []
-            for a in args:
-                cmd.add_argument(a)
-            cmd.set_defaults(func=wrap_cmd(action, args))
-
-    commands = {
-        'load_trips':         Command(load_trips, ['csv_dir']),
-        'geocode_stops':      Command(geocode_locations, []),
-        'plot_trips_by_hour': Command(plot_trips_by_hour, []),
-    }
-
-    argparser = argparse.ArgumentParser(prog='cambie')
-    load_cmds(argparser, commands)
-
-    args = argparser.parse_args()
-    args.func(args)
+    cli(env)
---
+++ cambie/argh.py
@@ -0,0 +1,41 @@
+from argparse import ArgumentParser
+from collections import namedtuple
+from itertools import chain
+
+from .prelude import items, juxt, thread, partial as p
+
+Command = namedtuple('Command', ['action', 'args', 'deps'])
+Command.__new__.__defaults__ = ((), ())
+
+def with_deps(f, deps=None):
+    """baby ur a **star"""
+    return lambda env, *a, **kw: f(*chain(juxt(*deps)(env) if deps else (),
+                                          *a),
+                                   **kw)
+
+def with_cmd_args(f, args=None):
+    return lambda ns, env: f(env=env,
+                             **{k: getattr(ns, k)
+                                for k in args or ()})
+
+def load_cmds(argparser, cmds):
+    _cmds = argparser.add_subparsers()
+
+    for name, cmd in items(cmds):
+        cmd_parser = _cmds.add_parser(name)
+        for a in cmd.args:
+            cmd_parser.add_argument(a)
+
+        cmd_parser.set_defaults(func=thread(cmd.action,
+                                            p(with_deps,     deps=cmd.deps),
+                                            p(with_cmd_args, args=cmd.args)))
+
+def argh(program_name, commands):
+    argparser = ArgumentParser(prog=program_name)
+    load_cmds(argparser, commands)
+
+    def _run(env):
+        args_ns = argparser.parse_args()
+        args_ns.func(args_ns, env=env)
+
+    return _run
