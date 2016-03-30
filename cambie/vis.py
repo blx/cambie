caac8a89b025844c88916c76010449de5394f1ab
Ben Cook <b@bencook.ca>
2016-03-29 20:30:24 -0700

    fix matplotlib graphs (hahahaha)

--- cambie/vis.py
+++ cambie/vis.py
@@ -1,13 +1,23 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+
+from .prelude import partial
 
 
-def _hist(bins, weights):
-    """Returns a pyplot histogram of `weights` labeled by `bins`."""
+def _bar(bins, weights):
+    """Returns a pyplot bar graph of `weights` labeled by `bins`."""
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    hist = ax.hist(bins,
-                   len(bins) - 1,
-                   weights=weights)
+    bar = ax.bar(left=bins,
+                 height=weights)
+    ax.set_xlim(left=0, right=24)
+    start, end = ax.get_xlim()
+    ax.xaxis.set_ticks(np.arange(start, end, 1.0))
+    ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%02.f'))
+    ax.tick_params(color='black',
+                   direction='out',
+                   length=5)
     return fig
 
 def trips_by_hour(db):
@@ -16,6 +26,10 @@ def trips_by_hour(db):
                                   from "trip" t
                                   join "location" l on t."location" = l."location"
                                   group by hour
-                                  order by n desc""")
+                                  order by hour asc""")
     bins, weights = zip(*data)
-    return _hist(bins, weights)
+    fig = _bar(bins, weights)
+    fig.axes[0].set_xlabel('Time of day')
+    fig.axes[0].set_ylabel('Tap-in count')
+    return fig
+
