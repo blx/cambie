776739975581c9694bc16af3c8045883ee2dad1a
Ben Cook <b@bencook.ca>
2016-03-29 22:17:28 -0700

    fix directions by hour

--- cambie/vis.py
+++ cambie/vis.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 
-from .prelude import partial
+from .prelude import *
 
 
 def _bar(bins, weights):
@@ -18,6 +18,7 @@ def _bar(bins, weights):
     ax.tick_params(color='black',
                    direction='out',
                    length=5)
+    ax.grid(True)
     return fig
 
 def trips_by_hour(db):
@@ -34,8 +35,17 @@ def trips_by_hour(db):
     return fig
 
 def _directions_by_hour(db, hour):
+    if not 0 <= hour <= 23:
+        raise IndexError('Hour must be within [0, 23]')
+
     time_lo = '%02d:30' % ((hour - 1) % 24)
     time_hi = '%02d:29' % hour
+
+    if hour != 0:
+        cond = "tm between (?) and (?)"
+    else:
+        cond = "tm >= (?) or tm <= (?)"
+
     data = db.cursor().execute("""
         select
             coalesce(sum(case when substr(z.name, 0, 3) == 'WB' then z.n end), 0) n_WB,
@@ -51,7 +61,7 @@ def _directions_by_hour(db, hour):
             where
                 (t."transaction" like 'Tap in%'
                  or t."transaction" like 'Transfer at%')
-                and (?) <= tm and tm <= (?)
+                and """ + cond + """
             group by t."location"
             order by n desc
         ) z""", [time_lo, time_hi])
@@ -60,6 +70,7 @@ def _directions_by_hour(db, hour):
 def directions_by_hour(db):
     hours = list(range(0, 24))
     dirs = map(comp(first,
+                    list,  # realize all the Cursors
                     partial(_directions_by_hour, db)),
                hours)
     return _bar(hours, dirs)
